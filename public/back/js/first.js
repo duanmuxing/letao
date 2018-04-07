70763f637eef98d107bcbdf8eba5fbd5e782e204
dxw <dxw@aaa.con>
2018-04-07 23:41:00 +0800

    大坑添加分类

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -12,8 +12,46 @@ $(function(){
 				pageSize:pageSize,
 			},
 			success:function(info){
-				console.log(info)
+				// console.log(info)
+				$(".lt_content tbody").html( template("userTp1",info) )
 			}
 		});
 	}
+
+	$("#addBtn").click(function(){
+		$("#addModal").modal("show");
+	});
+
+	$("#form").bootstrapValidator({
+		 // 配置图标
+	   	feedbackIcons: {
+	      valid: 'glyphicon glyphicon-ok',
+	      invalid: 'glyphicon glyphicon-remove',
+	      validating: 'glyphicon glyphicon-refresh'
+	    },
+	   	fields:{
+	   		categoryName:{
+	   			validators:{
+	   				notEmpty:{
+	   					message:"请输入一级分类名称"
+	   				}
+	   			}
+	   		}
+	   	}
+	});
+
+
+	$("#form").on("success.form.bv",function(e){
+	// e.preventDefault();
+		$.ajax({
+			type:"post",
+			url:'/category/addTopCategory',
+			data:$("#form").serialize(),
+			success:function(info){
+				console.log(111);
+
+			}
+		});
+	})
 });
+
