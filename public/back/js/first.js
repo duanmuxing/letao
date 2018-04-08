741dc3f5f59b5022321aaa31ebfa62c5885c92d8
dxw <dxw@aaa.con>
2018-04-08 10:04:57 +0800

    一级分类功能完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -2,7 +2,7 @@ $(function(){
 	var currentPage=1;
 	var pageSize=5;
 	render();
-
+// 渲染函数封装
 	function render(){
 		$.ajax({
 			type:"get",
@@ -13,15 +13,24 @@ $(function(){
 			},
 			success:function(info){
 				// console.log(info)
-				$(".lt_content tbody").html( template("userTp1",info) )
+				$(".lt_content tbody").html( template("userTp1",info) );
+				$("#paginator").bootstrapPaginator({
+					bootstrapMajorVersion:3,
+					currentPage:info.page,
+					totalPages:Math.ceil(info.total/info.size),
+					onPageClicked:function(a,b,c,page){
+						currentPage=page;
+						render();
+					}
+				});
 			}
 		});
 	}
-
+	// 点击添加按钮，添加分类模态框显示
 	$("#addBtn").click(function(){
 		$("#addModal").modal("show");
 	});
-
+// 添加分类表单验证
 	$("#form").bootstrapValidator({
 		 // 配置图标
 	   	feedbackIcons: {
@@ -40,18 +49,26 @@ $(function(){
 	   	}
 	});
 
-
+	// 添加分类
 	$("#form").on("success.form.bv",function(e){
-	// e.preventDefault();
+		// e.preventDefault();
 		$.ajax({
 			type:"post",
 			url:'/category/addTopCategory',
 			data:$("#form").serialize(),
 			success:function(info){
-				console.log(111);
-
+				console.log(info);
+				if(info.success){
+					$("#addModal").modal("hide");
+					render()
+				}
 			}
 		});
 	})
+
+	// 分页
+
+
+
 });
 
